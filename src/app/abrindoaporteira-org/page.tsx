14acf92a880886cdf7e12c4e341bb04ea4c7d128
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-01-22 16:03:35 +0000

    Refactor AbrindoAPorteira component to improve readability and maintainability

--- src/app/abrindoaporteira-org/page.tsx
+++ src/app/abrindoaporteira-org/page.tsx
@@ -7,29 +7,23 @@ import { Timeline } from "@/components/jan2025/captura/section4"
 import { Professors } from "@/components/jan2025/captura/section5"
 import { Awards } from "@/components/jan2025/captura/section6"
 
-export default function AbrindoAPorteirav3() {
-    return (
-        <main className="min-h-screen bg-white">
-            <HeroSection
-                title=" O que fez a Fazenda África se tornar uma das  maiores referências de agronegócio no Piauí?"
-                subtitle={
-                    <>
-                       Vamos revelar {" "}
-                        <span className="text-green-500">
-                            5 estratégias essenciais
-                        </span> 
-                        para estruturar uma fazenda do zero e garantir resultados impressionantes.
-                    </>
-                }
-                showPhone={false}
-                webhookLink="https://n8n.aesmkt.com.br/webhook/lzto-ap-fev25-org"
-            />
-            <ResultsSection />
-            <FeaturesGrid />
-            <Timeline />
-            <Professors />
-            <Awards />
-        </main>
-    )
+export default function AbrindoAPorteirav2() {
+  return (
+    <main className="min-h-screen bg-white">
+      <HeroSection
+        webhookLink="https://n8n.aesmkt.com.br/webhook/lzto-ap-fev25-org"
+        title="O que fez a Fazenda África se tornar uma das maiores referências de agronegócio no Piauí?"
+        subtitle={
+          <>
+            Vamos revelar 5 estratégias essenciais para estruturar uma fazenda do zero e garantir resultados impressionantes.
+          </>
+        }
+      />
+      <ResultsSection />
+      <FeaturesGrid />
+      <Timeline />
+      <Professors />
+      <Awards />
+    </main>
+  )
 }
-
